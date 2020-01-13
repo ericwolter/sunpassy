178dcaa76f479a9c9b547d76b908139b20975559
Eric <eric.wolter@gmx.de>
2020-01-13 14:23:33 +0100

    forces html minification

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,14 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
-      minify: true
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
     }),
     new WebpackPwaManifest({
       name: 'sunpassy',
